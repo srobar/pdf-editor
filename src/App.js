a8c16c62405bc77d2013a3f9e71ab13287879314
srobar <tomas.vavro91@gmail.com>
2024-09-10 09:47:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import './App.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import samplePDF from './sample.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 function App() {
+  const documentRef = useRef()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Document 
+        onLoadSuccess={(e) => { 
+          documentRef.current = e._transport;
+        }} 
+        file={samplePDF}
+      >
+        <Page scale={1.5} renderAnnotationLayer={true} renderForms={true} pageNumber={1} />
+      </Document>
+      <button 
+        className='save-button'
+        onClick={async () => {
+          const data = await documentRef.current.saveDocument();
+          const blob = new Blob([data], { type: 'application/pdf' });
+    
+          const link = document.createElement('a');      
+          const url = URL.createObjectURL(blob);
+          link.href = url;
+          link.download = 'document.pdf';
+          
+          link.click();
+    
+          URL.revokeObjectURL(url);
+        }}>
+          {'Save file'}
+      </button>
     </div>
   );
 }
